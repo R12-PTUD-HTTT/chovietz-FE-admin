9ca606bb90e083b0c6e2fd7acaf14a93b923982f
nguyễn văn hải <48937890+vanhai28@users.noreply.github.com>
2021-12-08 22:41:24 +0700

    set up router and redux

---
+++ src/views/commons/Login/LoginPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./styleLogin.module.css";
+
+function LoginPage(props) {
+  return <div className={style.LoginPage}>write any thing here</div>;
+}
+
+export default LoginPage;
