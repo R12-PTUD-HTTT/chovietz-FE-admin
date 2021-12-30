0feb81722d9249b00f5055d310193cce8b597f3d
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2021-12-30 16:14:39 +0700

    Add UI for sign up shipper

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,11 +4,13 @@ import SecureView from "./routers/SecureView";
 import HomePage from "./views/commons/home/HomePage";
 import LoginPage from "./views/commons/Login/LoginPage";
 import NotFoundPage from "./views/commons/Error/NotFoundPage";
+import SignupShipper from "./views/commons/signupShipper/SignupShipper";
 function Layout(props) {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/signup-shipper" component={SignupShipper} />
       {SecureView()}
       <Route component={NotFoundPage} />
     </Switch>
