f3242de8cf6a48e7a4e87a039b9cd272388fcb8b
Khoa <dangkhoa2401bg@gmail.com>
2022-01-19 10:44:22 +0700

    fix UI Dang ky ban hang

--- src/views/commons/home/HomePage.jsx
+++ src/views/commons/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Navbar } from "react-bootstrap";
 import { setPageHeder } from "../../../redux/actions/pageAction";
-
+import NavBar from "../../../components/NavBar/NarBar"
 function HomePage(props) {
   setPageHeder("Dashboard");
   return (
     <>
+    <NavBar/>
       <h2>Home</h2>
     </>
   );
